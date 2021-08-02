494351360bd6780eabcecba3fcf3cd6b1e0d6c76
NautilusCarpenter <nautilus.carpenter@gmail.com>
2021-08-02 23:09:25 +0100

    Strategy: Opening Range Breakout

--- ORB.py
+++ ORB.py
@@ -1,7 +1,7 @@
 from base import Base_App
 
-ib_acct = "DU" #update the ib account (different from real account and paper account)
-tickers = [] #ticker list would be updated by the scannerDara wrapper function
+ib_acct = "DU1234567" 
+tickers = []
 pos_size = 3000
 profit_limit = 1000
 loss_limit = -500
